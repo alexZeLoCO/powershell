c4436e535bf3ccb3e64b6f7947ee7950480bf4b6
unknown <rubennmg@github.com>
2023-05-02 13:56:08 +0200

    Update

--- FullComputerInfo.ps1
+++ FullComputerInfo.ps1
@@ -1,50 +1,65 @@
-﻿Write-Host "----------------------------- Identificación del equipo ---------------------------------"
+﻿$c = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName .
+$c.Name + "_Info.txt"
+$fpath = "C:\Temp"
+$full = $fpath + "\" + $outFile
+
+if (!(Test-Path $fpath)) {
+    Write-Host "El directorio no existe"
+    Exit
+}
+
+if (Test-Path $full) {
+    Write-Host "El fichero ya existe."
+    Exit
+}
+
+Add-Content -Path $full -value ("{0,-30}" -f "----------------------------- Identificación del equipo ---------------------------------")
 $a = Get-CimInstance –ClassName Win32_ComputerSystem –NameSpace Root\CIMV2 –Property *
-Write-Host "Nombre de equipo:" $a.Name
-Write-Host "Dominio:" $a.Domain
-Write-Host "----------------------------- Identificación del sistema operativo ----------------------"
+Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Nombre de equipo:", $a.Name)
+Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Dominio:", $a.Domain)
+Add-Content -Path $full -value ("{0,-30}" -f  "----------------------------- Identificación del sistema operativo ----------------------")
 $a = Get-CimInstance -ClassName Win32_OperatingSystem -Property *
-Write-Host "Edición de Windows" $a.Caption
-Write-Host "Fecha de instalación" $a.InstallDate
-Write-Host "Directorio de Windows" $a.SystemDirectoryWrite-Host "Arquitectura del SO" $a.OSArchitectureswitch ($a.PCSystemType){    0 { Write-Host "Tipo de producto: Sin Especificar" }    1 { Write-Host "Tipo de producto: Escritorio" }    2 { Write-Host "Tipo de producto: Movil" }    3 { Write-Host "Tipo de producto: Estación de Trabajo" }    4 { Write-Host "Tipo de producto: Enterprise Server" }    5 { Write-Host "Tipo de producto: Servidor SOHO" }    6 { Write-Host "Tipo de producto: PC del dispositivo" }    7 { Write-Host "Tipo de producto: Servidor de rendimiento" }    8 { Write-Host "Tipo de producto: Máximo" }}
-Write-Host "----------------------------- Listado de discos -----------------------------------------"
+Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Edición de Windows", $a.Caption)
+Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Fecha de instalación", $a.InstallDate)
+Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Directorio de Windows", $a.SystemDirectory)Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Arquitectura del SO", $a.OSArchitecture)switch ($a.PCSystemType){    0 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: Sin Especificar") }    1 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: Escritorio") }    2 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: Movil") }    3 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: Estación de Trabajo") }    4 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: Enterprise Server") }    5 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: Servidor SOHO") }    6 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: PC del dispositivo") }    7 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: Servidor de rendimiento") }    8 { Add-Content -Path $full -value ("{0,-30}" -f  "Tipo de producto: Máximo") }}
+Add-Content -Path $full -value ("{0,-30}" -f  "----------------------------- Listado de discos -----------------------------------------")
 $a = Get-CimInstance –ClassName Win32_DiskDrive –Namespace Root\CIMV2 -ComputerName .
 if ($a.GetType().IsArray) 
 {
-    Write-Host "Nº de discos" $a.Length
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Nº de discos", $a.Length)
 }
 else
 {
-    Write-Host "Nº de discos 1"
+    Add-Content -Path $full -value ("{0,-30}" -f  "Nº de discos 1")
 }
 
 $idx = 0
 foreach ($vol in $a)
 {
-    Write-Host "============ Disco " $idx " ======="
-    Write-Host "Nombre" $vol.Name
-    Write-Host "Modelo" $vol.model
-    Write-Host "Tamaño (en GB)" $vol.Size
+    Add-Content -Path $full -value ("{0,-30} {1,0} {1, -60}" -f  "============ Disco ", $idx, " =======")
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Nombre", $vol.Name)
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Modelo", $vol.model)
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Tamaño (en GB)", $vol.Size)
     $idx = $idx + 1
 }
-Write-Host "----------------------------- Listado de volúmenes --------------------------------------"
+Add-Content -Path $full -value ("{0,-30}" -f  "----------------------------- Listado de volúmenes --------------------------------------")
 $a = Get-CimInstance –ClassName Win32_Volume –Namespace Root\CIMV2 -ComputerName .
 if ($a.GetType().IsArray) 
 {
-    Write-Host "Nº de volumenes" $a.Length
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f  "Nº de volumenes", $a.Length)
 }
 else
 {
-    Write-Host "Nº de volumenes 1"
+    Add-Content -Path $full -value ("{0,-30}" -f  "Nº de volumenes 1")
 }
 $idx = 0
 foreach ($vol in $a)
 {
-    Write-Host "============ Volumen " $idx " ======="
-    Write-Host "Letra Asignada" $vol.DriveLetter
-    Write-Host "Capacidad (en MB)" $vol.Capacity
-    Write-Host "Espacio libre (en MB)"  $vol.FreeSpace
-    Write-Host "Tipo de drive" $vol.DriveType
-    Write-Host "Sistema de Ficheros" $vol.FileSystem
+    Add-Content -Path $full -value ("{0,-30} {1,0} {1, -60}" -f  "============ Volumen ", $idx, " =======")
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f "Letra Asignada", $vol.DriveLetter)
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f "Capacidad (en MB)", $vol.Capacity)
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f "Espacio libre (en MB)",  $vol.FreeSpace)
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f "Tipo de drive", $vol.DriveType)
+    Add-Content -Path $full -value ("{0,-30} {1,-60}" -f "Sistema de Ficheros", $vol.FileSystem)
     $idx = $idx + 1
 }
---
+++ GestionaServicio.ps1
@@ -0,0 +1,52 @@
+﻿$serviceName = Read-Host "Introduzca el nombre del servicio a gestionar"
+
+if ((Get-CimInstance -Query "SELECT * FROM Win32_Service WHERE name = '$serviceName'" -NameSpace root/cimv2) -eq $null) {
+    Write-Host "El servicio no existe"
+    Exit
+}
+
+Write-Host "Seleccione una opción"
+Write-Host "1) Obtener una descripcion del servicio"
+Write-Host "2) Indicar modo de arranque"
+Write-Host "3) Indicar estado del servicio"
+Write-Host "4) Arrancar el servicio"
+Write-Host "5) Parar el servicio"
+
+$option = Read-Host "Introduzca el numero de la opcion debe estar entre 1 y 5."
+if ($option -lt 1 -or $option -gt 5) {
+    Write-Host "Opcion Invalida. El numero de opción debe estar entre 1 y 5"
+    Exit
+}
+
+switch ($option) {
+    1 {
+        $service = Get-CimInstance -Class Win32_Service -Filter "Name='$serviceName'"
+        Write-Host "Descripcion del cometido del servicio '$serviceName': $($service.Description)"
+    }
+    2 {
+        $service = Get-CimInstance -Class Win32_Service -Filter "Name='$serviceName'"
+        Write-Host "Modo de arranque del servicio '$serviceName': $($service.Description)"
+    }
+    3 {
+        $service = Get-CimInstance -Class Win32_Service -Filter "Name='$serviceName'"
+        Write-Host "Estado del servicio '$serviceName': $($service.State)"
+    }
+    4 {
+        $service = Get-CimInstance -Class Win32_Service -Filter "Name='$serviceName'"
+        $out = Invoke-CimMethod -InputObject $service -MethodName StartService
+        if ($out.ReturnValue -eq 0) {
+            Write-Host "El servico ha sido iniciado"
+        } else {
+            Write-Host "Error al iniciar el servicio '$serviceName'."
+        }
+    }
+    5 {
+        $service = Get-CimInstance -Class Win32_Service -Filter "Name='$serviceName'"
+        $out = Invoke-CimMethod -InputObject $service -MethodName StopService
+        if ($out.ReturnValue -eq 0) {
+            Write-Host "El servico ha sido detenido"
+        } else {
+            Write-Host "Error al detener el servicio '$serviceName'."
+        }
+    }
+}
